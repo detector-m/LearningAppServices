b2bcbdc983af5126872b151915658bab671e3033
Riven <lrw90316@163.com>
2020-07-17 21:27:48 +0800

    新增文件

---
+++ LearningAppServices/crawler/stock.py
@@ -0,0 +1,15 @@
+# *-* coding: utf-8 *-
+
+import requests
+import re
+from bs4 import BeautifulSoup
+import traceback
+
+def crawl():
+    print('hello')
+
+def getHtmlContentText(url, encode='utf-8'):
+    pass
+
+if __name__ == '__main__':
+    crawl()
